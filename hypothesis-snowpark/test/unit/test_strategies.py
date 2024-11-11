068ac9020d1fe030baf378bddf2876b762966b4e
Fabian Gonzalez Mendez <f.gonzalezmendez@snowflake.com>
2024-11-11 17:35:06 -0600

    Add pytest optional dependency

--- hypothesis-snowpark/test/unit/test_strategies.py
+++ hypothesis-snowpark/test/unit/test_strategies.py
@@ -1,10 +1,8 @@
-import unittest
 from hypothesis import given
 from snowflake.snowpark import DataFrame
 from snowflake.hypothesis_snowpark import snowpark_dataframe
 
 
-class TestSnowparkDfStrategy(unittest.TestCase):
-    @given(df=snowpark_dataframe())
-    def test_with_snowpark_dataframe(self, df: DataFrame):
-        self.assertEqual(df.count(), 4)
+@given(df=snowpark_dataframe())
+def test_with_snowpark_dataframe(df: DataFrame):
+    assert df.count() == 5

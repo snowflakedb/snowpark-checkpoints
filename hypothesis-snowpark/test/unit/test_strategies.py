747973a4108b42e808ed7b99e59c3eeadd252947
Fabian Gonzalez Mendez <f.gonzalezmendez@snowflake.com>
2024-11-13 09:29:51 -0600

    Extend test example

--- hypothesis-snowpark/test/unit/test_strategies.py
+++ hypothesis-snowpark/test/unit/test_strategies.py
@@ -1,13 +1,24 @@
-from hypothesis import given
+from hypothesis import given, settings
 from snowflake.snowpark import DataFrame
-from snowflake.snowpark.types import StructField, BooleanType, StringType
+from snowflake.snowpark.types import StructField, BooleanType, StringType, IntegerType
 
 from snowflake.hypothesis_snowpark import snowpark_dataframe
 
 
-@given(df=snowpark_dataframe(
-    columns=[StructField("id", BooleanType()),
-             StructField("name", StringType())]))
-def test_with_snowpark_dataframe(df: DataFrame):
+@given(
+    df=snowpark_dataframe(
+        columns=[
+            StructField("A", IntegerType()),
+            StructField("B", StringType()),
+            StructField("C", BooleanType()),
+        ],
+        min_rows=1,
+        max_rows=10,
+    )
+)
+@settings(max_examples=5)
+def test_snowpark_dataframe_strategy(df: DataFrame):
+    assert isinstance(df, DataFrame)
+    assert len(df.schema.fields) == 3
+    assert 1 <= df.count() <= 10
     df.show()
-    assert df.count() >= 5

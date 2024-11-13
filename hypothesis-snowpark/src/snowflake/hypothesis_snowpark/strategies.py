6e3c67481c805d6adeb5dae0e2879d533cf3d4ef
Fabian Gonzalez Mendez <f.gonzalezmendez@snowflake.com>
2024-11-13 09:38:16 -0600

    Code cleanup

--- hypothesis-snowpark/src/snowflake/hypothesis_snowpark/strategies.py
+++ hypothesis-snowpark/src/snowflake/hypothesis_snowpark/strategies.py
@@ -28,18 +28,15 @@ def text(draw):
 
 @st.composite
 def snowpark_dataframe(
-        draw: Callable[[SearchStrategy], T],
-        *,
-        columns: Sequence[StructField],
-        min_rows: int = 5,
-        max_rows: int = 20,
-        session: Optional[snowflake.snowpark.Session] = None
+    draw: Callable[[SearchStrategy], T],
+    *,
+    columns: Sequence[StructField],
+    min_rows: int = 5,
+    max_rows: int = 20,
+    session: Optional[snowflake.snowpark.Session] = None,
 ) -> DataFrame:
     """
     Defines a hypothesis strategy to generate Snowpark :class:`snowflake.snowpark.DataFrame`.
-
-
-
 
     :param draw: Special function that allows to generate values from other strategies within this custom strategy.
     :param columns: required parameter a Sequence of Snowpark StructFields with the column definition.
@@ -49,26 +46,23 @@ def snowpark_dataframe(
     :return: a Snowpark DataFrame.
     """
 
-    # Data type that we should support: integers, booleans, strings, bytes, null
-    # Proposed method signature:
-    # def snowpark_dataframe(
-    #     draw: Callable[[SearchStrategy], T],
-    #     columns: List[StructField],
-    #     session: Session = None,
-    # ): ...
-
     candidate_st = []
     for col in columns:
         if type(col.datatype) in snowpark_st:
             col_strategy = snowpark_st[type(col.datatype)]
-            print(type(col_strategy))
             candidate_st.append(col_strategy)
         else:
-            raise Exception(f'Not Implemented SearchStrategy for column {col.name} of type {col.datatype}')
-    print(candidate_st)
-    data = draw(st.lists((st.tuples(*candidate_st)), min_size=min_rows, max_size=max_rows))
+            raise Exception(f"Not Implemented SearchStrategy for column {col.name} of type {col.datatype}")
 
-    session = Session.builder.config("local_testing", True).create() if session is None else session
+    data = draw(
+        st.lists((st.tuples(*candidate_st)), min_size=min_rows, max_size=max_rows)
+    )
+
+    session = (
+        Session.builder.config("local_testing", True).create()
+        if session is None
+        else session
+    )
 
     df = session.create_dataframe(data, schema=StructType([*columns]))
 

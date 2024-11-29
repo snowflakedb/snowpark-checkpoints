e785f4802b695f927b7ac6cdc9ccc16f4834f11b
Diego Babb Jimenez <diego.babbjimenez@snowflake.com>
2024-11-29 11:25:07 -0600

    Fix import path in supported_types.py for consistency

--- snowpark-checkpoints/src/snowflake/snowpark_checkpoints/utils/supported_types.py
+++ snowpark-checkpoints/src/snowflake/snowpark_checkpoints/utils/supported_types.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
-from .constant import (
+from snowflake.snowpark_checkpoints.utils.constant import (
     BINARY_TYPE,
     BOOLEAN_TYPE,
     BYTE_TYPE,

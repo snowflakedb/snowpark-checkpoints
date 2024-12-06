d51ffd20abfeff912af499572206aff2c967ee3c
Diego Chacon <d.chaconbarrantes@snowflake.com>
2024-12-06 15:08:21 -0600

    add documentation in line

--- snowpark-checkpoints/src/snowflake/snowpark_checkpoints/utils/telemetry.py
+++ snowpark-checkpoints/src/snowflake/snowpark_checkpoints/utils/telemetry.py
@@ -25,6 +25,7 @@ from snowflake.snowpark_checkpoints.utils.singleton import Singleton
 
 class TelemetryManager(metaclass=Singleton):
     def __init__(self):
+        """TelemetryManager class to log telemetry events."""
         self.folder_path = str(
             snowflake_dirs.user_config_path / "snowpark-checkpoints-telemetry"
         )
@@ -37,13 +38,38 @@ class TelemetryManager(metaclass=Singleton):
         self.log_batch = []
 
     def log_error(self, event_name: str, parameters_info: dict = None):
+        """Log an error telemetry event.
+
+        Args:
+            event_name (str): The name of the event.
+            parameters_info (dict, optional): Additional parameters for the event. Defaults to None.
+
+        """
         self._log_telemetry(event_name, "error", parameters_info)
 
     def log_info(self, event_name: str, parameters_info=None):
+        """Log an information telemetry event.
+
+        Args:
+            event_name (str): The name of the event.
+            parameters_info (dict, optional): Additional parameters for the event. Defaults to None.
+
+        """
         if randint(1, 100) <= 5:
             self._log_telemetry(event_name, "info", parameters_info)
 
     def _log_telemetry(self, event_name: str, event_type, parameters_info=None) -> dict:
+        """Log a telemetry event if is enabled.
+
+        Args:
+            event_name (str): The name of the event.
+            event_type (str): The type of the event (e.g., "error", "info").
+            parameters_info (dict, optional): Additional parameters for the event. Defaults to None.
+
+        Returns:
+            dict: The logged event.
+
+        """
         if not self.is_enabled:
             return None
         event = _generate_event(event_name, event_type, self.conn, parameters_info)
@@ -51,12 +77,27 @@ class TelemetryManager(metaclass=Singleton):
         return event
 
     def _add_log_to_batch(self, event: dict) -> None:
+        """Add a log event to the batch. If the batch is full, send the events to the API.
+
+        Args:
+            event (dict): The event to add.
+
+        """
         self.log_batch.append(event)
         if len(self.log_batch) >= self.flush_size:
             self._send_batch(self.log_batch)
             self.log_batch = []
 
     def _send_batch(self, to_sent: list) -> bool:
+        """Send a request to the API to upload the events. If not have connection, write the events to local folder.
+
+        Args:
+            to_sent (list): The batch of events to send.
+
+        Returns:
+            bool: True if the batch was sent successfully, False otherwise.
+
+        """
         if not self.is_enabled:
             return False
         if self.conn.rest is None:
@@ -76,6 +117,12 @@ class TelemetryManager(metaclass=Singleton):
         return True
 
     def _write_telemetry(self, batch: list) -> None:
+        """Write telemetry events to local folder. If the folder is full, free up space for the new events.
+
+        Args:
+            batch (list): The batch of events to write.
+
+        """
         makedirs(self.folder_path, exist_ok=True)
         for event in batch:
             message = event.get("message")
@@ -89,6 +136,15 @@ class TelemetryManager(metaclass=Singleton):
                     json_file.write(json_content)
 
     def _validate_folder_space(self, event: dict):
+        """Validate and manage folder space for the new telemetry events.
+
+        Args:
+            event (dict): The event to validate.
+
+        Returns:
+            str: The JSON content of the event.
+
+        """
         json_content = json.dumps(event, indent=4, sort_keys=True)
         new_json_file_size = len(json_content.encode("utf-8"))
         telemetry_folder = Path(self.folder_path)
@@ -98,6 +154,7 @@ class TelemetryManager(metaclass=Singleton):
         return json_content
 
     def _upload_local_telemetry(self) -> None:
+        """Send a request to the API to upload the local telemetry events."""
         batch = []
         for file in Path(self.folder_path).glob("*.json"):
             with open(file) as json_file:
@@ -116,6 +173,12 @@ class TelemetryManager(metaclass=Singleton):
                 file.unlink()
 
     def _is_telemetry_enabled(self) -> bool:
+        """Check if telemetry is enabled.
+
+        Returns:
+            bool: True if telemetry is enabled, False otherwise.
+
+        """
         if getenv("SNOWPARK_CHECKPOINTS_TELEMETRY_ENABLED") == "false":
             return False
         return self.conn.telemetry_enabled
@@ -127,6 +190,18 @@ def _generate_event(
     conn: SnowflakeConnection,
     parameters_info: dict = None,
 ) -> dict:
+    """Generate a telemetry event.
+
+    Args:
+        event_name (str): The name of the event.
+        event_type (str): The type of the event (e.g., "error", "info").
+        conn (SnowflakeConnection): The Snowflake connection object.
+        parameters_info (dict, optional): Additional parameters for the event. Defaults to None.
+
+    Returns:
+        dict: The generated event.
+
+    """
     metadata = _get_metadata()
     message = {
         "type": event_type,
@@ -144,6 +219,12 @@ def _generate_event(
 
 
 def _get_metadata() -> dict:
+    """Get metadata for telemetry events.
+
+    Returns:
+        dict: The metadata including OS version, Python version, and device ID.
+
+    """
     return {
         "OS_Version": platform,
         "Python_Version": python_version(),
@@ -152,10 +233,26 @@ def _get_metadata() -> dict:
 
 
 def _get_folder_size(folder_path: Path) -> int:
+    """Get the size of a folder. Only considers JSON files.
+
+    Args:
+        folder_path (Path): The path to the folder.
+
+    Returns:
+        int: The size of the folder in bytes.
+
+    """
     return sum(f.stat().st_size for f in folder_path.glob("*.json") if f.is_file())
 
 
 def _free_up_space(folder_path: Path, max_size: int) -> None:
+    """Free up space in a folder by deleting the oldest files. Only considers JSON files.
+
+    Args:
+        folder_path (Path): The path to the folder.
+        max_size (int): The maximum allowed size of the folder in bytes.
+
+    """
     files = sorted(folder_path.glob("*.json"), key=lambda f: f.stat().st_mtime)
     current_size = _get_folder_size(folder_path)
     for file in files:
@@ -167,6 +264,12 @@ def _free_up_space(folder_path: Path, max_size: int) -> None:
 
 
 def _get_unique_id() -> str:
+    """Get a unique device ID. The ID is generated based on the hashed MAC address.
+
+    Returns:
+        str: The hashed device ID.
+
+    """
     node_id_str = str(getnode())
     hashed_id = hashlib.sha256(node_id_str.encode()).hexdigest()
     return hashed_id

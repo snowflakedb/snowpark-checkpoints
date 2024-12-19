c4e326f606bf6ca46d4b0d9a6542f73b16dd7850
Kelvin Jimenez <kelvin.jimenez@snowflake.com>
2024-12-19 13:21:46 -0600

    Expose only the CheckpointMetadata class as is the only one, which should be exposed.

--- snowpark-checkpoints-configuration/src/snowflake/snowpark_checkpoints_configuration/__init__.py
+++ snowpark-checkpoints-configuration/src/snowflake/snowpark_checkpoints_configuration/__init__.py
@@ -1,3 +1,11 @@
 #
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
+
+from snowflake.snowpark_checkpoints_configuration.checkpoint_metadata import (
+    CheckpointMetadata,
+)
+
+__all__ = [
+    "CheckpointMetadata",
+]

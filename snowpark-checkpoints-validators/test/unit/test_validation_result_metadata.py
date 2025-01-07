71162ea30487378b06aa577bf2bf4bc080840715
Diego Babb Jimenez <diego.babbjimenez@snowflake.com>
2025-01-07 14:51:57 -0600

    Support for validation result file (#27)

---
+++ snowpark-checkpoints-validators/test/unit/test_validation_result_metadata.py
@@ -0,0 +1,82 @@
+import json
+import os
+from unittest.mock import mock_open, patch
+from snowflake.snowpark_checkpoints.utils.constant import (
+    PASS_STATUS,
+    VALIDATION_RESULTS_JSON_FILE_NAME,
+)
+from snowflake.snowpark_checkpoints.validation_result_metadata import (
+    ValidationResultsMetadata,
+)
+from snowflake.snowpark_checkpoints.validation_results import (
+    ValidationResult,
+    ValidationResults,
+)
+
+
+def test_load_with_valid_file():
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(
+        path, "checkpoint_validation_results", VALIDATION_RESULTS_JSON_FILE_NAME
+    )
+    with open(path) as file:
+        validation_result_json = file.read()
+        mock_validation_results = ValidationResults.model_validate_json(
+            validation_result_json
+        )
+
+    metadata = ValidationResultsMetadata(path)
+
+    assert metadata.validation_results == mock_validation_results
+
+
+def test_load_with_no_file():
+    metadata = ValidationResultsMetadata()
+
+    assert metadata.validation_results == ValidationResults(results=[])
+
+
+def test_add_validation_result():
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(
+        path,
+        "checkpoint_validation_results",
+        "non_existent_file.json",
+    )
+
+    metadata = ValidationResultsMetadata(path)
+
+    new_validation_result = ValidationResult(
+        timestamp="2021-01-01T00:00:00",
+        file="file2",
+        line_of_code=1,
+        result=PASS_STATUS,
+        checkpoint_name="checkpoint2",
+    )
+
+    metadata.add_validation_result(new_validation_result)
+
+    assert metadata.validation_results == ValidationResults(
+        results=[new_validation_result]
+    )
+
+
+def test_save_success():
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(
+        path, "checkpoint_validation_results", VALIDATION_RESULTS_JSON_FILE_NAME
+    )
+    with open(path) as file:
+        validation_results_json = file.read()
+        mock_validation_results = ValidationResults.model_validate_json(
+            validation_results_json
+        )
+
+    metadata = ValidationResultsMetadata(path)
+    with patch("builtins.open", mock_open()) as mock_open_file:
+        metadata.save()
+
+    mock_open_file().write.assert_called_once_with(
+        mock_validation_results.model_dump_json()
+    )

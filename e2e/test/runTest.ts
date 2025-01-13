bd78a4fd6d38afe8015a7b9c9a8a7b22e8a990f6
Daniel Acuna Solano <daniel.acunasolano@snowflake.com>
2025-01-13 12:57:49 -0600

    Configure e2e

---
+++ e2e/test/runTest.ts
@@ -0,0 +1,39 @@
+import * as cp from 'child_process';
+import * as path from 'path';
+import {
+  downloadAndUnzipVSCode,
+  resolveCliArgsFromVSCodeExecutablePath,
+  runTests
+} from '@vscode/test-electron';
+
+async function main() {
+  try {
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.78.0');
+    const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    // Use cp.spawn / cp.exec for custom setup
+    cp.spawnSync(
+      cliPath,
+      [...args, '--install-extension', './extension'],
+      {
+        encoding: 'utf-8',
+        stdio: 'inherit'
+      }
+    );
+
+    // Run the extension test
+    await runTests({
+      // Use the specified `code` executable
+      vscodeExecutablePath,
+      extensionDevelopmentPath,
+      extensionTestsPath
+    });
+  } catch (err) {
+    console.error('Failed to run tests');
+    process.exit(1);
+  }
+}
+
+main();

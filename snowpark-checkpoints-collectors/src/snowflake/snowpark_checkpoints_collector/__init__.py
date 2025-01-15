f253ec11bd43f1c781d7a50d55feb3c58fcc13cb
Luis Bravo Zuniga <luis.bravozuniga@snowflake.com>
2025-01-15 09:31:55 -0600

    Reverting changes

--- snowpark-checkpoints-collectors/src/snowflake/snowpark_checkpoints_collector/__init__.py
+++ snowpark-checkpoints-collectors/src/snowflake/snowpark_checkpoints_collector/__init__.py
@@ -2,9 +2,8 @@
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
-__all__ = ["collect_dataframe_checkpoint", "Singleton", "CheckpointMode"]
+__all__ = ["collect_dataframe_checkpoint", "Singleton"]
 
-from snowflake.snowpark_checkpoints_collector.collection_common import CheckpointMode
 from snowflake.snowpark_checkpoints_collector.singleton import Singleton
 from snowflake.snowpark_checkpoints_collector.summary_stats_collector import (
     collect_dataframe_checkpoint,

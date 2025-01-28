9ddd3577f82a1a0ed08e55a5df55771706e80fc9
Diego Chacon <d.chaconbarrantes@snowflake.com>
2025-01-28 09:17:03 -0600

    Handle exception in telemetry when there is no Snowflake connection

--- snowpark-checkpoints-collectors/test/integ/telemetry_compare_utils.py
+++ snowpark-checkpoints-collectors/test/integ/telemetry_compare_utils.py
@@ -20,17 +20,6 @@ def get_output_telemetry(telemetry_directory_path: Path) -> str:
 def validate_telemetry_file_output(
     telemetry_file_name: str, output_path: Path, telemetry_expected_folder: str
 ) -> None:
-    telemetry_output = get_output_telemetry(output_path)
-    current_directory_path = os.path.dirname(__file__)
-    expected_file_path = os.path.join(
-        current_directory_path,
-        telemetry_expected_folder,
-        telemetry_file_name,
-    )
-
-    with open(expected_file_path, "w") as f:
-        f.write(telemetry_output)
-
     telemetry_expected = get_expected(telemetry_file_name, telemetry_expected_folder)
     telemetry_output = get_output_telemetry(output_path)
     telemetry_expected_obj = json.loads(telemetry_expected)

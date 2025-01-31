ac5771f970f06a1caf971009f2774f71d0c406f4
Yerlyn Guerrero Leon <yerlyn.guerreroleon@snowflake.com>
2025-01-31 08:50:09 -0600

    Suggestions applied

--- snowpark-checkpoints-testing/test/e2e/test_e2e_checkpoints.py
+++ snowpark-checkpoints-testing/test/e2e/test_e2e_checkpoints.py
@@ -1,6 +1,17 @@
-#
-# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
-#
+# Copyright 2025 Snowflake Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 from pathlib import Path
@@ -64,7 +75,7 @@ def test_e2e_checkpoints(json_name_list, execution_mode, telemetry) -> None:
         dir = os.getcwd()
     ) as temp_dir:
         temp_path = Path(temp_dir)
-        telemetry_path = temp_path / "telemetry_data"
+        telemetry_path = os.path.join(temp_dir, "telemetry_data")
         telemetry.set_sc_output_path(telemetry_path)
         input_e2e_test_pyspark(execution_mode, temp_path)
         input_e2e_test_snowpark(execution_mode, temp_path)

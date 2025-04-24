f759e392bb7dda400146805b4b856aa1fe30f536
Diego Chacon <d.chaconbarrantes@snowflake.com>
2025-04-24 10:51:36 -0600

    fix test

--- snowpark-checkpoints-configuration/test/unit/io_utils/test_default_strategy.py
+++ snowpark-checkpoints-configuration/test/unit/io_utils/test_default_strategy.py
@@ -104,7 +104,7 @@ def test_default_set_strategy(io_file_manager):
     try:
         # Arrange
         with patch(
-            "snowflake.snowpark_checkpoints.io_utils.EnvStrategy"
+            "snowflake.snowpark_checkpoints_configuration.io_utils.EnvStrategy"
         ) as MockStrategy:
             strategy = MockStrategy()
 
